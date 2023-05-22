11747fefc42c9d300c4713c43b95e19dbcb25383
Mushbt <mustafa_habet@hotmail.com>
2023-05-22 12:58:53 +0000

    Test

--- student.py
+++ student.py
@@ -1,14 +1,15 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
-    """ A student class as base for method testing """
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
-        self._end_date = date.today() + timedelta(days=365)
+        self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
     @property
@@ -21,3 +22,14 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
